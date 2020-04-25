ad06925a2f1ab489b95ae78aa78283ac30480527
kirinnee <kirinnee97@gmail.com>
2020-04-25 16:37:46 +0800

    [Feat] Finished fibonacci

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::stdin;
 use std::collections::HashMap;
+use std::io::stdin;
 
 fn main() {
     loop {
@@ -16,16 +16,8 @@ fn main() {
         let mut cache = HashMap::new();
         cache.insert(1, 1);
         let fib = fib(n, &mut cache);
-//        let fib = slow_fib(n);
         println!("Value: {}", fib);
     }
-}
-
-fn slow_fib(x: i32) -> u64 {
-    if x <= 1 {
-        return x as u64;
-    }
-    slow_fib(x - 1) + slow_fib(x - 2)
 }
 
 fn fib(x: i32, cache: &mut HashMap<i32, u64>) -> u64 {
