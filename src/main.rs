d7dda2f92a9a545c7590446cff1f099fc58c556c
kirinnee <kirinnee97@gmail.com>
2020-04-26 00:00:18 +0800

    [Temp]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use std::io::stdin;
 
 fn main() {
-    println!("Hello World!");
+    let ordinal = ["first", "second", "third", "fourth", "fifth", "sixth",
+        "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"];
+}
+
+fn day(day: usize) -> String {
+    let phrase = ["A partridge in a pear tree", "Two turtle doves",
+        "Three French hens", "Four calling birds", "Five gold rings",
+        "Six geese a laying", "Seven swans a swimming", "Eight maids a milking",
+        "Nine ladies dancing", "Ten lords a leaping", "Eleven pipers piping",
+        "Twelve drummers drumming",
+    ];
+
+    match day {
+        0 => phrase[0].to_string(),
+        1 => (phrase[1].to_string() + " and " + phrase[2].to_ascii_lowercase())
+    }
 }
 
 
