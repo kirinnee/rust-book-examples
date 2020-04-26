f883521ea8cb50a4e7a5aea71e5d3048e2618415
kirinnee <kirinnee97@gmail.com>
2020-04-26 15:22:06 +0800

    [Feat] Added prompting option

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,58 @@
 use std::io::stdin;
 
-fn main() {
-    let ordinal = ["first", "second", "third", "fourth", "fifth", "sixth",
-        "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"];
+const PHRASE: [&str; 12] = ["A partridge in a pear tree", "Two turtle doves",
+    "Three French hens", "Four calling birds", "Five gold rings",
+    "Six geese a laying", "Seven swans a swimming", "Eight maids a milking",
+    "Nine ladies dancing", "Ten lords a leaping", "Eleven pipers piping",
+    "Twelve drummers drumming",
+];
 
-    let mut x = 0;
-    while x < 12 {
-        println!("On the {} of Christmas my true love gave to me", ordinal[x]);
-        println!("{}", day(x));
-        x += 1;
+const ORDINAL: [&str; 12] = ["first", "second", "third", "fourth", "fifth", "sixth",
+    "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"];
+
+fn ask_for_days() -> usize {
+    loop {
+        println!("How many days do you want: ");
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("Cannot read line");
+
+        match input.trim().parse() {
+            Ok(e) => break match e {
+                1..=12 => e,
+                _ => {
+                    println!("Must be from 1 to 12");
+                    continue;
+                }
+            }
+            ,
+            Err(e) => {
+                println!("{}", e);
+                continue;
+            }
+        }
+    }
+}
+
+fn main() {
+    loop {
+        let mut x = 0;
+        let end = ask_for_days();
+        while x < end {
+            println!("On the {} of Christmas my true love gave to me", ORDINAL[x]);
+            println!("{}", day(x));
+            x += 1;
+        }
     }
 }
 
 fn day(day: usize) -> String {
-    let phrase = vec!["A partridge in a pear tree", "Two turtle doves",
-                      "Three French hens", "Four calling birds", "Five gold rings",
-                      "Six geese a laying", "Seven swans a swimming", "Eight maids a milking",
-                      "Nine ladies dancing", "Ten lords a leaping", "Eleven pipers piping",
-                      "Twelve drummers drumming",
-    ];
-
     match day {
-        0 => phrase[0].to_string(),
-        1 => (phrase[1].to_string() + " and " + phrase[0].to_ascii_lowercase().as_str()).to_string(),
+        0 => PHRASE[0].to_string(),
+        1 => (PHRASE[1].to_string() + " and " + PHRASE[0].to_ascii_lowercase().as_str()).to_string(),
         _ => {
-            let phrases: Vec<_> = phrase[1..(day)].iter().map(|x| { x.to_ascii_lowercase() }).rev().collect();
+            let phrases: Vec<_> = PHRASE[1..(day)].iter().map(|x| { x.to_ascii_lowercase() }).rev().collect();
             let p: String = phrases.join(", ");
-            phrase[day].to_string() + ", " + p.as_str() + " and " + phrase[0].to_ascii_lowercase().as_str()
+            PHRASE[day].to_string() + ", " + p.as_str() + " and " + PHRASE[0].to_ascii_lowercase().as_str()
         }
     }
 }
