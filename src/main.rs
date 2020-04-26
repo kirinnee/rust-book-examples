cc9a91c6bd5d6c47eeafba0de29d6139b8f821cb
kirinnee <kirinnee97@gmail.com>
2020-04-26 15:35:43 +0800

    [Feat] Added loop prompt function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use std::io::stdin;
 
+fn ask_for_input<F>(question: &str, f: F) -> String where F: Fn(&str) -> bool {
+    loop {
+        println!("{}", question);
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("Failed to read line");
+        let out = input.trim();
+        if f(out) {
+            break out.to_string();
+        } else {
+            continue;
+        }
+    }
+}
+
 fn main() {
     println!("Hello World!");
 }
