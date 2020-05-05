943aa844bc6bd7e1cbbee93275e7bfe2793597e8
kirinnee <kirinnee97@gmail.com>
2020-05-05 18:52:05 +0800

    median max and mode

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::io::stdin;
+use std::num::ParseIntError;
 
 fn ask_for_input<F, T>(question: &str, f: F) -> T where F: (Fn(&str) -> Result<T, String>) {
     loop {
@@ -17,7 +20,45 @@ fn ask_for_input<F, T>(question: &str, f: F) -> T where F: (Fn(&str) -> Result<T
 }
 
 fn main() {
-    println!("Hello World!");
+    let asker = |x: &str| -> Result<Vec<i64>, String> {
+        x.split_ascii_whitespace()
+            .map(|x|
+                x.parse()
+                    .map_err(|err: ParseIntError| err.to_string()))
+            .collect()
+    };
+
+    loop {
+        let mut v = ask_for_input("Enter an array for integers", asker);
+        let sum: i64 = v.iter().sum();
+        let avg = sum as f32 / v.len() as f32;
+
+        v.sort();
+        let median = v[v.len() / 2];
+
+        let mut occurence = HashMap::new();
+
+        for i in v.into_iter() {
+            let count = occurence.entry(i).or_insert(0);
+            *count += 1;
+        }
+
+        println!("Average: {}", avg);
+        println!("Median: {}", median);
+
+        if let Some((k, v)) = occurence.into_iter()
+            .max_by(|(_, av), (_, bv)|
+                if *av > *bv {
+                    Ordering::Greater
+                } else {
+                    Ordering::Less
+                }
+            ) {
+            println!("Max occurrence: {} with {} times", k, v);
+        } else {
+            println!("no max");
+        }
+    }
 }
 
 
