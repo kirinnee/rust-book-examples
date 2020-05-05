c60eac36e4383c6642e84c7da9e49cb060f3baff
kirinnee <kirinnee97@gmail.com>
2020-05-05 19:17:06 +0800

    convert english to pig latin

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,30 @@ fn ask_for_input<F, T>(question: &str, f: F) -> T where F: (Fn(&str) -> Result<T
 }
 
 fn main() {
-    println!("Hello World!");
+    loop {
+        let s = ask_for_input("Input a phrase: ", |x| match x.len() {
+            0 => Err("Please enter a string".to_string()),
+            _ => Ok(x.to_string()),
+        });
+
+        let mut iter = s.chars();
+        let y = iter.next();
+        let mut left = iter.collect::<String>();
+        if let Some(c) = y {
+            let output = match c {
+                'a' | 'e' | 'i' | 'o' | 'u' => {
+                    let out = format!("{}{}-hay", c, left);
+                    out
+                }
+                _ => {
+                    left.push('-');
+                    left.push(c);
+                    left + "ay"
+                }
+            };
+            println!("Pig latin: {}", output)
+        }
+    }
 }
 
 
