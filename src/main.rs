19eede6c38dee80143bc9913ec0c72e35182320c
kirinnee <kirinnee97@gmail.com>
2020-05-07 13:29:50 +0800

    box and deref

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::io::stdin;
+use std::ops::Deref;
 
+use crate::List::{Cons, Nil};
+
+#[allow(dead_code)]
 fn ask_for_input<F, T>(question: &str, f: F) -> T where F: (Fn(&str) -> Result<T, String>) {
     loop {
         println!("{}", question);
@@ -16,8 +20,36 @@ fn ask_for_input<F, T>(question: &str, f: F) -> T where F: (Fn(&str) -> Result<T
     }
 }
 
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+
+#[derive(Debug)]
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
 fn main() {
-    println!("Hello World!");
+    let b = MyBox::new(5);
+    println!("b = {}", *b);
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    println!("Cons list: {:?}", list)
 }
 
 
