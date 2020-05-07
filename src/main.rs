97609b659bc45e6735f120d765d5a30f8c83a8e2
kirinnee <kirinnee97@gmail.com>
2020-05-07 16:46:25 +0800

    Rc RfCell

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,13 @@
-use std::io::stdin;
+use std::cell::RefCell;
+use std::fmt::{Display, Error, Formatter};
+use std::ops::Deref;
 use std::rc::Rc;
 
 use crate::List::{Cons, Nil};
 
-#[allow(dead_code)]
-fn ask_for_input<F, T>(question: &str, f: F) -> T where F: (Fn(&str) -> Result<T, String>) {
-    loop {
-        println!("{}", question);
-        let mut input = String::new();
-        stdin().read_line(&mut input).expect("Failed to read line");
-        let out = input.trim();
-        match f(out) {
-            Ok(e) => break e,
-            Err(e) => {
-                println!("{}", e);
-                continue;
-            }
-        }
-    }
-}
-
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
@@ -29,29 +15,56 @@ impl List {
     fn nil() -> Rc<List> {
         Rc::new(Nil)
     }
-    fn new(i: i32, next: &Rc<List>) -> Rc<List> {
-        Rc::new(Cons(i, Rc::clone(next)))
+    fn new(i: &Rc<RefCell<i32>>, next: &Rc<List>) -> Rc<List> {
+        Rc::new(Cons(Rc::clone(i), Rc::clone(next)))
+    }
+    fn num(i: i32) -> Rc<RefCell<i32>> {
+        Rc::new(RefCell::new(i))
     }
 }
 
-
+impl Display for List {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        let mut current = self;
+        let mut s = vec![];
+        loop {
+            match current {
+                Cons(value, next) => {
+                    current = next;
+                    let i = *value.deref().borrow();
+                    s.push(i.to_string())
+                }
+                Nil => {
+                    break write!(f, "{{{}}}", s.join(","));
+                }
+            }
+        }
+    }
+}
 
 fn main() {
-    let a = List::new(1, &List::nil());
-    let b = List::new(2, &a);
+    let value = List::num(1);
+    let a = List::new(&value, &List::nil());
+    let b = List::new(&List::num(2), &a);
     println!("created b, count {}", Rc::strong_count(&b));
-    let c = List::new(3, &b);
+    let c = List::new(&List::num(3), &b);
     println!("created c, count {}", Rc::strong_count(&b));
-    let d = List::new(4, &b);
+    let d = List::new(&List::num(4), &b);
     println!("created d, count {}", Rc::strong_count(&b));
 
-
     {
-        let e = List::new(5, &b);
+        let e = List::new(&List::num(5), &b);
         println!("created e, count {}", Rc::strong_count(&b));
     }
+
     println!("deleted e, count {}", Rc::strong_count(&b));
+
+    *value.borrow_mut() += 10;
+
+    println!("d {}", d);
+    println!("c {}", c);
 }
+
 
 
 
