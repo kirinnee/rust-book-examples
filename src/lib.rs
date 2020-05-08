958c433262cbed718624bd045c4a4d144ee48881
kirinnee <kirinnee97@gmail.com>
2020-05-08 20:35:32 +0800

    generic tree structure

---
+++ src/lib.rs
@@ -0,0 +1,84 @@
+use std::cell::{RefCell, RefMut};
+use std::fmt::{Debug, Display, Error, Formatter};
+use std::rc::{Rc, Weak};
+
+#[derive(Debug)]
+pub struct Node<T> {
+    pub value: RefCell<T>,
+    pub parent: RefCell<Weak<Node<T>>>,
+    pub children: RefCell<Vec<Rc<Node<T>>>>,
+    self_node: RefCell<Weak<Node<T>>>,
+}
+
+impl<T: Display> Display for Node<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        let s = if self.children.borrow().len() > 0 {
+            String::from("[    \n") + &(self.children
+                .borrow()
+                .iter()
+                .map(|x| {
+                    let y: Vec<String> = (*x).to_string()
+                        .split('\n')
+                        .map(|x| "        ".to_string() + x).collect();
+                    y.join("\n")
+                }
+                )
+                .collect::<Vec<String>>()
+                .join(",\n")) + "\n    ]\n}"
+        } else {
+            "[] }".to_string()
+        };
+
+        write!(f, "{{ {}, {}", *self.value.borrow(), s)
+    }
+}
+
+impl<T: Display> Node<T> {
+    pub fn new(value: T) -> Rc<Node<T>> {
+        let x = Rc::new(Node {
+            value: RefCell::new(value),
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![]),
+            self_node: RefCell::new(Weak::new()),
+        });
+        *x.self_node.borrow_mut() = Rc::downgrade(&x);
+        x
+    }
+
+    pub fn add_parent(&self, a: &Rc<Node<T>>) {
+        *self.parent() = Rc::downgrade(a);
+        a.children().push(Rc::clone(&(*self.self_node.borrow()).upgrade().unwrap()));
+    }
+
+    pub fn add_child(&self, a: &Rc<Node<T>>) {
+        (*self.children()).push(Rc::clone(a));
+        *a.parent() = Rc::downgrade(&(*self.self_node.borrow()).upgrade().unwrap());
+    }
+
+    pub fn children_val(&self) -> String {
+        self.children.borrow()
+            .iter()
+            .map(|x| (*(**x).value.borrow()).to_string())
+            .collect::<Vec<_>>()
+            .join(", ")
+    }
+
+    pub fn parent_val(&self) -> Option<String> {
+        let parent = self.parent.borrow_mut().upgrade()?;
+        let temp = Rc::clone(&parent);
+        let val = &temp.value.borrow().to_string();
+        Some(val.to_string())
+    }
+
+    pub fn children(&self) -> RefMut<Vec<Rc<Node<T>>>> {
+        self.children.borrow_mut()
+    }
+
+    pub fn parent(&self) -> RefMut<Weak<Node<T>>> {
+        self.parent.borrow_mut()
+    }
+
+    pub fn value(&self) -> RefMut<T> {
+        self.value.borrow_mut()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use std::fmt::{Debug, Display};
 use std::io::stdin;
+use std::rc::Rc;
+
+use colour::*;
+
+use guessing_game::Node;
 
 #[allow(dead_code)]
 fn ask_for_input<F, T>(question: &str, f: F) -> T where F: (Fn(&str) -> Result<T, String>) {
@@ -18,8 +24,167 @@ fn ask_for_input<F, T>(question: &str, f: F) -> T where F: (Fn(&str) -> Result<T
 }
 
 fn main() {
-    println!("Hello World!");
+//    manual_building();
+    auto_build();
 }
+
+fn auto_build() {
+    // building a tree
+    //
+    //               8  h
+    //               /  \
+    //             /     \
+    //           /        \
+    //         /           \
+    //       /              \
+    //     6 f             7  g
+    //      /\            / | \
+    //     /  \         /  |   \
+    //    /    \      /   |     \
+    //  1 a   2 b   3 c  4 d     5 e
+    let a = Node::new(1);
+    let b = Node::new(2);
+    let c = Node::new(3);
+    let d = Node::new(4);
+    let e = Node::new(5);
+    let f = Node::new(6);
+    let g = Node::new(7);
+    let h = Node::new(8);
+
+
+    h.add_child(&f);
+    h.add_child(&g);
+
+    f.add_child(&a);
+    f.add_child(&b);
+
+    g.add_child(&c);
+    g.add_child(&d);
+    g.add_child(&e);
+
+
+    println!();
+    print("a =", &a);
+    print("b =", &b);
+    print("c =", &c);
+    print("d =", &d);
+    print("e =", &e);
+    print("f =", &f);
+    print("g =", &g);
+    print("h =", &h);
+
+
+    println!();
+    print("children of a =", a.children_val());
+    print("children of b = ", b.children_val());
+    print("children of c = ", c.children_val());
+    print("children of d = ", d.children_val());
+    print("children of e = ", e.children_val());
+    print("children of f = ", f.children_val());
+    print("children of g = ", g.children_val());
+    print("children of h = ", h.children_val());
+
+
+    println!();
+    printd("parent of a = ", a.parent_val());
+    printd("parent of b = ", b.parent_val());
+    printd("parent of c = ", c.parent_val());
+    printd("parent of d = ", d.parent_val());
+    printd("parent of e = ", e.parent_val());
+    printd("parent of f = ", f.parent_val());
+    printd("parent of g = ", g.parent_val());
+    printd("parent of h = ", h.parent_val());
+}
+
+fn manual_building() {
+    // building a tree
+    //
+    //               8  h
+    //               /  \
+    //             /     \
+    //           /        \
+    //         /           \
+    //       /              \
+    //     6 f             7  g
+    //      /\            / | \
+    //     /  \         /  |   \
+    //    /    \      /   |     \
+    //  1 a   2 b   3 c  4 d     5 e
+    let a = Node::new(1);
+    let b = Node::new(2);
+    let c = Node::new(3);
+    let d = Node::new(4);
+    let e = Node::new(5);
+    let f = Node::new(6);
+    let g = Node::new(7);
+    let h = Node::new(8);
+
+    {
+        let mut f_child = f.children();
+        f_child.push(Rc::clone(&a));
+        *a.parent() = Rc::downgrade(&f);
+        f_child.push(Rc::clone(&b));
+        *b.parent() = Rc::downgrade(&f);
+
+        let mut g_child = g.children();
+        g_child.push(Rc::clone(&c));
+        *c.parent() = Rc::downgrade(&g);
+        g_child.push(Rc::clone(&d));
+        *d.parent() = Rc::downgrade(&g);
+        g_child.push(Rc::clone(&e));
+        *e.parent() = Rc::downgrade(&g);
+
+        let mut h_child = h.children();
+        h_child.push(Rc::clone(&f));
+        *f.parent() = Rc::downgrade(&h);
+        h_child.push(Rc::clone(&g));
+        *g.parent() = Rc::downgrade(&h);
+    }
+
+    println!();
+    print("a =", &a);
+    print("b =", &b);
+    print("c =", &c);
+    print("d =", &d);
+    print("e =", &e);
+    print("f =", &f);
+    print("g =", &g);
+    print("h =", &h);
+
+
+    println!();
+    print("children of a =", a.children_val());
+    print("children of b = ", b.children_val());
+    print("children of c = ", c.children_val());
+    print("children of d = ", d.children_val());
+    print("children of e = ", e.children_val());
+    print("children of f = ", f.children_val());
+    print("children of g = ", g.children_val());
+    print("children of h = ", h.children_val());
+
+
+    println!();
+    printd("parent of a = ", a.parent_val());
+    printd("parent of b = ", b.parent_val());
+    printd("parent of c = ", c.parent_val());
+    printd("parent of d = ", d.parent_val());
+    printd("parent of e = ", e.parent_val());
+    printd("parent of f = ", f.parent_val());
+    printd("parent of g = ", g.parent_val());
+    printd("parent of h = ", h.parent_val());
+}
+
+fn printd(target: &str, any: impl Debug) {
+    dark_cyan_ln!(target);
+    println!("{:?}", any)
+}
+
+fn print(target: &str, any: impl Display) {
+    dark_cyan_ln!(target);
+    println!("{}", any)
+}
+
+
 
 
 
