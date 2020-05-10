a794b1d0f841df4777b86caf431df4e7818e0dc9
kirinnee <kirinnee97@gmail.com>
2020-05-10 21:48:11 +0800

    unsafe rust

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,45 @@
-use std::io::stdin;
+use core::slice;
 
-#[allow(dead_code)]
-fn ask_for_input<F, T>(question: &str, f: F) -> T where F: (Fn(&str) -> Result<T, String>) {
-    loop {
-        println!("{}", question);
-        let mut input = String::new();
-        stdin().read_line(&mut input).expect("Failed to read line");
-        let out = input.trim();
-        match f(out) {
-            Ok(e) => break e,
-            Err(e) => {
-                println!("{}", e);
-                continue;
-            }
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
+fn main() {
+    let mut arr = [
+        "a".to_string(),
+        "ab".to_string(),
+        "abc".to_string(),
+        "abcde".to_string(),
+        "abcdef".to_string(),
+    ];
+
+    let ptr = arr[..].as_mut_ptr();
+
+    unsafe {
+        for i in 0..5 {
+            let cptr = ptr.add(i);
+            println!("Address = {:?}, value = {:?}", cptr, *cptr);
         }
     }
-}
 
-fn main() {
-    println!("Hello World!");
-}
+    let mut x = 5;
+    unsafe {
+        let x1 = &mut x as *mut i32;
+        *x1 = 1234;
+        let x2 = x1.add(1);
+        *x2 = 12345;
+        let x3 = x2.add(1);
+        *x3 = 123456;
+    }
 
+    let slice = unsafe {
+        let r = &mut x as *mut i32;
+        slice::from_raw_parts_mut(r, 3)
+    };
+    println!("slice: {:?}", slice);
+
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
+    }
+}
 
 
