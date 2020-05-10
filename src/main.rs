cc2f9c9c12d082eab02a9ffae42ef757be7aa5ed
kirinnee <kirinnee97@gmail.com>
2020-05-10 13:21:35 +0800

    channels

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,39 @@
-use std::io::stdin;
-
-#[allow(dead_code)]
-fn ask_for_input<F, T>(question: &str, f: F) -> T where F: (Fn(&str) -> Result<T, String>) {
-    loop {
-        println!("{}", question);
-        let mut input = String::new();
-        stdin().read_line(&mut input).expect("Failed to read line");
-        let out = input.trim();
-        match f(out) {
-            Ok(e) => break e,
-            Err(e) => {
-                println!("{}", e);
-                continue;
-            }
-        }
-    }
-}
+use std::sync::mpsc;
+use std::sync::mpsc::Sender;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    println!("Hello World!");
+    let (tx1, rx) = mpsc::channel();
+
+    let tx2 = Sender::clone(&tx1);
+
+    thread::spawn(move || {
+        let vals: Vec<_> = vec!["hi", "this", "is", "ernest"]
+            .iter()
+            .map(|x| (*x).to_string())
+            .collect();
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals: Vec<_> = vec!["hi", "this", "is", "cc"]
+            .iter()
+            .map(|x| (*x).to_string())
+            .collect();
+
+        for val in vals {
+            tx2.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
 
 
